cbede34efe3d970750328dd82e2d792a23ecf4ba
Majoemyles <majormilesvigour@gmail.com>
2024-12-07 11:55:37 +0000

    update

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,10 @@
-import axios from 'axios'
+import axios from 'axios';
+
+const baseURL = process.env.REACT_APP_API_URL || 'https://marketpulse-api.onrender.com';
+
 const api = axios.create({
-    baseURL: 'https://marketpulse-api.onrender.com/api'
-})
-export default api
+    baseURL: `${baseURL}/api`,
+    withCredentials: true
+});
+
+export default api;
